52a8d70aaea06a17fdf4df519518831247f3007c
fakhroni <fakhroni.daeriza12@gmail.com>
2023-06-30 15:17:51 +0700

    login

---
+++ app/src/main/java/com/fakhronidaeriza/login/MahasiswaAdapter.java
@@ -0,0 +1,29 @@
+package com.fakhronidaeriza.login;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MahasiswaAdapter extends RecyclerView.Adapter {
+    public MahasiswaAdapter(TampilMahasiswaActivity tampilMahasiswaActivity, List<MahasiswaModel> mahasiswaModelList) {
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
